ac48b9cf9c38a908fedde21ac26c926bf1914c54
Inmaculada Concepcion <inmaculadaconcepcionmeneses@gmail.com>
2025-05-10 20:29:47 +0200

    se hace documentacion de los modelos correspondientes a objetos "no controlables"

--- mazmorra/src/main/java/com/mazmorras/models/Camino.java
+++ mazmorra/src/main/java/com/mazmorras/models/Camino.java
@@ -1,5 +1,8 @@
 package com.mazmorras.models;
 
+//La clase Camino representa una celda de camino en una mazmorra o laberinto.
+//Extiende la clase Celda, heredando sus propiedades y comportamientos, se utiliza para definir un tipo específico de celda que puede
+//ser atravesada dentro de la estructura del laberinto o mazmorra.
 public class Camino extends Celda {
     public Camino (int x, int y){
         super(x, y);
--- mazmorra/src/main/java/com/mazmorras/models/Juego.java
+++ mazmorra/src/main/java/com/mazmorras/models/Juego.java
@@ -7,6 +7,15 @@ import java.util.ArrayList;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 
+//La clase Juego} implementa la interfaz y representa
+//un modelo de juego que permite a los oyentes ser notificados de cambios.
+//Esta clase mantiene una lista de objetos que pueden
+//ser añadidos o eliminados para observar cambios en el estado del juego.
+//addListener(InvalidationListener)}: Añade un oyente a la lista si no es nulo y no está ya presente.
+//removeListener(InvalidationListener)}: Elimina un oyente de la lista.
+//getTurnoActual: Recupera el turno actual (nota: este método puede tener un problema de recursión infinita).
+//El método getTurnoActual parece llamarse a sí mismo recursivamente, lo que puede llevar a un StackOverflowError. 
+//Esto debería ser revisado y corregido.
 public class Juego implements Observable {
 
     private List<InvalidationListener> listeners;
